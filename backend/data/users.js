093a3ac524e953cbce0d45fdc6d2c0b11b896b00
innosoltech <innovativesolgh@gmail.com>
2024-01-11 16:10:16 +0000

    update to files

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,18 +2,18 @@ import bcrypt from "bcryptjs";
 
 const users = [
   {
-    name: "Admin User",
+    name: "Caleb Selormey (test)",
     email: "admin@email.com",
     password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
   {
-    name: "John Doe",
+    name: "John Doe (test)",
     email: "john@email.com",
     password: bcrypt.hashSync("123456", 10),
   },
   {
-    name: "Jane Doe",
+    name: "Jane Doe (test)",
     email: "jane@email.com",
     password: bcrypt.hashSync("123456", 10),
   },
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,11 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
+  : {
+      cartItems: [],
+      shippingAddress: {},
+      paymentMethod: "Card (debit/credit/prepaid)",
+    };
 
 const cartSlice = createSlice({
   name: "cart",
