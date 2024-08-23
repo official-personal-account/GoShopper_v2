7e846923f709f08c9471f527da87d7bf245bfb6f
innosoltech <innovativesolgh@gmail.com>
2024-08-23 18:32:10 +0000

    updates made

--- frontend/src/screens/InfoScreen.jsx
+++ frontend/src/screens/InfoScreen.jsx
@@ -21,6 +21,7 @@ const InfoScreen = () => {
               even complete their order offline.
             </p>
 
+            <br />
             <h5>If you are a small business owner</h5>
             <p>
               Proshopper saves you time, and allows you to focus on other
@@ -32,6 +33,7 @@ const InfoScreen = () => {
               in other things.
             </p>
 
+            <br />
             <h5>Proshopper video demo</h5>
             <p>
               Watch a live recorded video demo of proshopper in the sequence it
@@ -50,6 +52,7 @@ const InfoScreen = () => {
               </span>
             </p>
 
+            <br />
             <h5>Testing & Exploring Proshopper</h5>
             <p>
               Where required to log in, in order to complete an order, use any
