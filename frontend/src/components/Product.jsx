03e8bb3f92d75abc8009049464cb22083ce4b796
caselormey <caselormey@gmail.com>
2025-08-01 18:25:55 +0000

    updates made

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,24 +7,24 @@ const Product = ({ product }) => {
     <Card className="my-3 p-0 rounded">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" className="fitObject" />
+
+        <Card.Body>
+          <Link to={`/product/${product._id}`}>
+            <Card.Title as="div" className="product-title">
+              <strong>{product.name}</strong>
+            </Card.Title>
+          </Link>
+
+          <Card.Text as="div">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
+          </Card.Text>
+
+          <Card.Text as="h3">₵{product.price}</Card.Text>
+        </Card.Body>
       </Link>
-
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title as="div" className="product-title">
-            <strong>{product.name}</strong>
-          </Card.Title>
-        </Link>
-
-        <Card.Text as="div">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
-
-        <Card.Text as="h3">₵{product.price}</Card.Text>
-      </Card.Body>
     </Card>
   );
 };
