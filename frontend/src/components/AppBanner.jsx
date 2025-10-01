ea68608b54e40adda65ec2e6a4516b22fb27936f
caselormey <caselormey@gmail.com>
2025-10-01 23:17:46 +0000

    updates made

--- frontend/src/components/AppBanner.jsx
+++ frontend/src/components/AppBanner.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { LuDot } from "react-icons/lu";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const AppBanner = () => {
   return (
@@ -7,20 +8,28 @@ const AppBanner = () => {
       style={{
         background: "rgba(255, 18, 18)",
         textAlign: "center",
-        height: "25px",
+        height: "30px",
         padding: "2px",
       }}
     >
-      <p style={{ fontSize: "small", color: "white" }}>
+      <p style={{ fontSize: "small", color: "white", display: "inline-block" }}>
         Customisable <LuDot /> Easy to use
-        <Link
-          to="/info"
-          style={{ color: "white", textDecorationColor: "white" }}
-        >
-          <LuDot />
-          Learn more
-        </Link>
-      </p>
+      </p>{" "}
+      <Link
+        to="/info"
+        style={{
+          color: "white",
+          textDecorationColor: "white",
+          textDecoration: "none",
+          fontSize: "small",
+          // float: "right",
+          marginLeft: "25px",
+          letterSpacing: "1px",
+        }}
+        className="twinkling-element"
+      >
+        Learn more <FaArrowRightLong />
+      </Link>
     </div>
   );
 };
